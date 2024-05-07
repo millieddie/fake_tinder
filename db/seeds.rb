ebdace9f42bcbfb52695de8d6669ed349f3a8ad6
millieddie <w231435.ak@gmail.com>
2024-05-07 23:08:40 +0900

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,129 @@
+p "create Users"
+User.create!(
+  email: 'user1@gmail.com',
+  password: '11111111',
+  name: 'ルフィー',
+  self_introduction: '海賊王に俺はなる',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/luffy.png")
+)
+User.create!(
+  email: 'user2@gmail.com',
+  password: '11111111',
+  name: 'ゾロ',
+  self_introduction: '背中の傷は剣士の恥だ',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/zoro.png")
+)
+User.create!(
+  email: 'user3@gmail.com',
+  password: '11111111',
+  name: 'サンジ',
+  self_introduction: '人は ”心”だろうが！！！！',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/sanji.png")
+)
+User.create!(
+  email: 'user4@gmail.com',
+  password: '11111111',
+  name: 'ロビン',
+  self_introduction: '私は…生ぎたい！ 私も一緒に、海へ連れてって！',
+  gender: 1,
+  profile_image: open("#{Rails.root}/db/dummy_images/robin.png")
+)
+User.create!(
+  email: 'user5@gmail.com',
+  password: '11111111',
+  name: 'ウソップ',
+  self_introduction: 'だからおれは海へ出ようと決意した!!',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/usopp.png")
+)
+User.create!(
+  email: 'user6@gmail.com',
+  password: '11111111',
+  name: 'ルッチ',
+  self_introduction: 'クルッポー',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/lucci.png")
+)
+User.create!(
+  email: 'user7@gmail.com',
+  password: '11111111',
+  name: 'アーロン',
+  self_introduction: 'シャハハハハ！！',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/arlong.png")
+)
+User.create!(
+  email: 'user8@gmail.com',
+  password: '11111111',
+  name: 'ブルック',
+  self_introduction: '私…美人に目がないんです！！…ガイコツだから目はないんですけども！！ヨホホホホ！！',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/brook.png")
+)
+User.create!(
+  email: 'user9@gmail.com',
+  password: '11111111',
+  name: 'バギー',
+  self_introduction: '誰がつけっ鼻だよ！',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/buggy.png")
+)
+User.create!(
+  email: 'user10@gmail.com',
+  password: '11111111',
+  name: 'チョッパー',
+  self_introduction: 'そうだよトナカイだ！！！',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/chopper.png")
+)
+User.create!(
+  email: 'user11@gmail.com',
+  password: '11111111',
+  name: 'クロコダイル',
+  self_introduction: '”理想”ってのは実力が伴う者のみ口にできる”現実”だ・・・！！',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/crocodile.png")
+)
+User.create!(
+  email: 'user12@gmail.com',
+  password: '11111111',
+  name: 'ドフラミンゴ',
+  self_introduction: '勝者だけが 正義だ！！！！',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/doflamingo.png")
+)
+User.create!(
+  email: 'user13@gmail.com',
+  password: '11111111',
+  name: 'エネル',
+  self_introduction: '我が神なり',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/enel.png")
+)
+User.create!(
+  email: 'user14@gmail.com',
+  password: '11111111',
+  name: 'フランキー',
+  self_introduction: 'なんのその男は裸百貫の 波に向かって立つ獅子であれ',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/franky.png")
+)
+User.create!(
+  email: 'user15@gmail.com',
+  password: '11111111',
+  name: 'ジンベエ',
+  self_introduction: 'この身を削って時間稼ぎになるなら結構 もとより命などくれてやるハラじゃい',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/jinbe.png")
+)
+User.create!(
+  email: 'user16@gmail.com',
+  password: '11111111',
+  name: 'ティーチ',
+  self_introduction: 'やめときな、正義だ悪だと口にするのは!!',
+  gender: 0,
+  profile_image: open("#{Rails.root}/db/dummy_images/kurohige_teach.png")
+)
